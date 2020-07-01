9ed591f6005797ca143f1596675451ffa6e3051f
dk <dktaak23@gmail.com>
2020-07-01 23:38:34 +0530

    sizeof

--- c.c
+++ c.c
@@ -2,6 +2,7 @@
 void main()
 {
 int num1,num2,num3,num=10,pos=3;
+char ch='s';
 //to clear bit
 num1=num&~(1<<3);
 //to set a bit
@@ -9,5 +10,5 @@ num2=num|1<<3;
 //to complement a bit
 num3=num^1<<3;
 printf("clear bit=%d\nset bit=%d\ncomplement bit=%d",num1,num2,num3);
-printf("%d\n%d\n%d\n%d\n",sizeof(int),sizeof(float),sizeof('s'),size of(ch));
+printf("\n%ld\n%ld\n%d\n%d\n",sizeof(int),sizeof(float),sizeof('s'),sizeof(ch));
 }
