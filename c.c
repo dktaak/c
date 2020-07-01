8044ea405f7af4a8e844855e8b17c17ed3f057f7
dk <dktaak23@gmail.com>
2020-07-01 20:25:26 +0530

    link c file to github

---
+++ c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int num1,num2,num3,num=10,pos=3;
+//to clear bit
+num1=num&~(1<<3);
+//to set a bit
+num2=num|1<<3;
+//to complement a bit
+num3=num^1<<3;
+printf("clear bit=%d\nset bit=%d\ncomplement bit=%d",num1,num2,num3);
+}
