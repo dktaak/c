bbf9b1f0a51d8bfb49281ffc3a29e75d57adba10
dk <dktaak23@gmail.com>
2020-07-01 23:25:20 +0530

    sizeof()

--- c.c
+++ c.c
@@ -9,4 +9,5 @@ num2=num|1<<3;
 //to complement a bit
 num3=num^1<<3;
 printf("clear bit=%d\nset bit=%d\ncomplement bit=%d",num1,num2,num3);
+printf("%d\n%d\n%d\n%d\n",sizeof(int),sizeof(float),sizeof('s'),size of(ch));
 }
