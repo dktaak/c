32e2a7a2d01d746557de962f4e078650e3ec3b44
dk <dktaak23@gmail.com>
2020-07-02 23:18:18 +0530

    to chk bit is clear or set

--- c.c
+++ c.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 void main()
 {
-int num1,num2,num3,num=10,pos=3;
-char ch='s';
+int num1,num2,num3,num,pos;
+printf("enter number");
+scanf("%d",&num);
+printf("enter position");
+scanf("%d",&pos);
+//char ch='s';
 //to clear bit
-num1=num&~(1<<3);
+num1=num&~(1<<pos);
 //to set a bit
-num2=num|1<<3;
+num2=num|1<<pos;
 //to complement a bit
-num3=num^1<<3;
+num3=num^1<<pos;
 printf("clear bit=%d\nset bit=%d\ncomplement bit=%d",num1,num2,num3);
-printf("\n%ld\n%ld\n%d\n%d\n",sizeof(int),sizeof(float),sizeof('s'),sizeof(ch));
+(num&1<<pos)>0?printf("\n%d bit is set",pos):printf("\n%d bit is clear",pos);
+//printf("\n%ld\n%ld\n%d\n%d\n",sizeof(int),sizeof(float),sizeof('s'),sizeof(ch));
 }
