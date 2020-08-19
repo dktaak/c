a5b5d6c7d6800cf9ff492797a8f581fe25f35e1b
dktaak <dktaak23@gmail.com>
2020-08-20 02:16:58 +0530

    copy string function

---
+++ mystrcpy/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mystrcpy(char*,const char*);
+int main()
+{
+    char s1[40],s2[20];
+    printf("Enter String s1,s2\n");
+    scanf("%s%s",s1,s2);
+    mystrcpy(s1,s2);
+    return 0;
+}
+void mystrcpy(char*s1,const char*s2)
+{
+    int i;
+    for(i=0;s1[i];i++);//find length of a string one
+    while(*s2)
+        s1[i++]=*s2++;
+    s1[i]=*s2;//copy \0
+    return 0;
+}
+
