aa188a512f081c7b7333ed2950f498ddd02ea1e5
dktaak <dktaak23@gmail.com>
2020-08-19 20:09:26 +0530

    practice

---
+++ practice/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+
+    char key;
+
+        char lock[10]={11,22,33,44,55,66};
+
+        key=(lock+1)[3];
+int a=10;
+        printf(“ %d ”,key);
+    return 0;
+}
